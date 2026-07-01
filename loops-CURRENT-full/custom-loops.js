b435929a2b6f828c8cd0e53e875fde3acd815bab
drsrennie-stack <dr.srennie@gmail.com>
2026-07-01 12:16:05 -0700

    add all loop images

---
+++ loops-CURRENT-full/custom-loops.js
@@ -0,0 +1,4 @@
+/* Loops you build with the Loop Builder live here.
+   The Loop Builder updates this file for you. You normally do not edit it by hand.
+   Each loop: { id, title, module, icon, unit, subtitle, dir, labels: [ "answer 1", "answer 2", ... ] } */
+window.CUSTOM_LOOPS = [];
